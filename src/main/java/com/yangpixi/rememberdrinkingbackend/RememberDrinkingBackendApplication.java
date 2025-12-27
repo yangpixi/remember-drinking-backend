9e0311596685aad81dd70d06e08876e4e35a2292
yangpixi <2602340701@qq.com>
2025-12-27 20:16:09 +0800

    Initial commit

---
+++ src/main/java/com/yangpixi/rememberdrinkingbackend/RememberDrinkingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yangpixi.rememberdrinkingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberDrinkingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RememberDrinkingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yangpixi/rememberdrinkingbackend/RememberDrinkingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangpixi.rememberdrinkingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememberDrinkingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
