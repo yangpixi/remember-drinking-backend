655f0528196462e59bdab30af669b7af05cb57f3
yangpixi <2602340701@qq.com>
2025-12-31 15:04:46 +0800

    add: 添加mybatis-plus依赖

--- src/main/java/com/yangpixi/rememberdrinkingbackend/RememberDrinkingBackendApplication.java
+++ src/main/java/com/yangpixi/rememberdrinkingbackend/RememberDrinkingBackendApplication.java
@@ -1,9 +1,11 @@
 package com.yangpixi.rememberdrinkingbackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yangpixi.rememberdrinkingbackend.mapper")
 public class RememberDrinkingBackendApplication {
 
     public static void main(String[] args) {
