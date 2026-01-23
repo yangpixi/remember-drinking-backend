7f66c234ebb45be8373bb6eb0b9472fedac82eff
yangpixi <2602340701@qq.com>
2026-01-23 20:01:48 +0800

    feat: 添加更改用户密码，手机号的接口

--- src/main/java/com/yangpixi/rememberdrinkingbackend/controller/UserController.java
+++ src/main/java/com/yangpixi/rememberdrinkingbackend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.yangpixi.rememberdrinkingbackend.controller;
 
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.yangpixi.rememberdrinkingbackend.dto.ApiResponse;
 import com.yangpixi.rememberdrinkingbackend.entity.User;
 import com.yangpixi.rememberdrinkingbackend.service.IUserService;
@@ -8,6 +9,7 @@ import com.yangpixi.rememberdrinkingbackend.vo.UserDetailsVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,6 +26,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class UserController {
 
     private final IUserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @PreAuthorize("hasAuthority('record:add')")
     @PostMapping("/avatar/change")
@@ -41,5 +44,27 @@ public class UserController {
         UserDetailsVO vo = new UserDetailsVO();
         BeanUtils.copyProperties(user, vo);
         return ApiResponse.success(vo);
+    }
+
+    @PreAuthorize("hasAuthority('record:add')")
+    @PostMapping("/phone/change")
+    public ApiResponse<String> changeUserPhone(@RequestParam("phone") String phone) {
+        User user = SecurityUtils.getCurrentUser().orElseThrow(() -> new RuntimeException("无法获取当前用户"));
+        UpdateWrapper<User> wrapper = new UpdateWrapper<>();
+        wrapper.eq("id", user.getId())
+                .set("phone", phone);
+        userService.update(wrapper);
+        return ApiResponse.success("修改手机号成功");
+    }
+
+    @PreAuthorize("hasAuthority('record:add')")
+    @PostMapping("/password/change")
+    public ApiResponse<String> changeUserPassword(@RequestParam("password") String password) {
+        User user = SecurityUtils.getCurrentUser().orElseThrow(() -> new RuntimeException("无法获取当前用户"));
+        UpdateWrapper<User> wrapper = new UpdateWrapper<>();
+        wrapper.eq("id", user.getId())
+                .set("password", passwordEncoder.encode(password));
+        userService.update(wrapper);
+        return ApiResponse.success("修改密码成功");
     }
 }
