1e267098002b575ca8136d3c103269e3bef20850
yangpixi <2602340701@qq.com>
2026-01-29 11:37:58 +0800

    chore: 添加用户取消喝水记录时的同步逻辑

--- src/main/java/com/yangpixi/rememberdrinkingbackend/service/impl/RecordServiceImpl.java
+++ src/main/java/com/yangpixi/rememberdrinkingbackend/service/impl/RecordServiceImpl.java
@@ -1,5 +1,6 @@
 package com.yangpixi.rememberdrinkingbackend.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.yangpixi.rememberdrinkingbackend.entity.Record;
 import com.yangpixi.rememberdrinkingbackend.mapper.RecordMapper;
@@ -7,6 +8,7 @@ import com.yangpixi.rememberdrinkingbackend.service.IRecordService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,7 +23,31 @@ public class RecordServiceImpl extends ServiceImpl<RecordMapper, Record> impleme
     @Override
     @Transactional
     public Boolean saveRecordList(List<Record> list) {
-        return this.saveBatch(list);
+        List<String> existingIds = this.list().stream().map(Record::getRecordId).toList();
+
+        List<Record> insertList = new ArrayList<>();
+        List<Record> updateList = new ArrayList<>();
+
+        for (Record record : list) {
+            if (existingIds.contains(record.getRecordId())) {
+                updateList.add(record);
+            } else {
+                insertList.add(record);
+            }
+        }
+
+        boolean saveState = this.saveBatch(insertList);
+        boolean updateState = true;
+
+        for (Record record : updateList) {
+            LambdaQueryWrapper<Record> wrapper = new LambdaQueryWrapper<>();
+            wrapper.eq(Record::getRecordId, record.getRecordId());
+            if (!this.update(record, wrapper)) {
+                updateState = false;
+            }
+        }
+
+        return saveState == updateState;
     }
 
 }
